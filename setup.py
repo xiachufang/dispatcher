5a7a4b8e63b4861f490d70fd950db7036f334113
Alex.F <gfreezy@gmail.com>
2017-03-14 14:49:49 +0800

    add code

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='pypydispatch',
+    maintainer='gfreezy',
+    maintainer_email='gfreezy@gmail.com',
+    description='A library for event-driven programming, extracted from Django',
+    packages=[
+        'dispatcher',
+    ],
+    version='1.0.3',
+    url='https://github.com/xiachufang/dispatcher',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
