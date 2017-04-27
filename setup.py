4fd45f69c1a096fde61b5f28d33be4c586f4168f
Alex.F <gfreezy@gmail.com>
2017-04-27 10:34:46 +0800

    tag 1.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=[
         'dispatcher',
     ],
-    version='1.0.10',
+    version='1.0.11',
     url='https://github.com/xiachufang/dispatcher',
     install_requires=[
         'celery',
