af84eb0f8f2a30c0bd67a22f71241c4f57e4508c
cadl <ctrlaltdeleteliu@gmail.com>
2023-03-07 16:45:05 +0800

    bump 1.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=[
         'dispatcher',
     ],
-    version='1.1.0',
+    version='1.2.0',
     url='https://github.com/xiachufang/dispatcher',
     install_requires=[
         'celery',
